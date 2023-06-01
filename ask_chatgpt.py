daa1f150c0f82abebd0505a8b89f9f4257ff88f1
Araray Velho <araray@gmail.com>
2023-06-01 19:25:12 -0300

    This commit makes the code more readable and maintainable by reorganizing the code. Also, there is now basic exception handling and more model parameters can be tweaked in the configuration file.

--- ask_chatgpt.py
+++ ask_chatgpt.py
@@ -1,25 +1,39 @@
-##
 ## Requires Python >= 3.11.0
 import sys
+from typing import List
 import tomllib
 
 from chatgpt import ChatGPT
 
-question = sys.argv[1]
 
-config = []
-with open("config.toml", "rb") as conf:
-    config = tomllib.load(conf)
+def load_config(file_path: str) -> dict:
+    with open(file_path, "rb") as conf_file:
+        return tomllib.load(conf_file)
 
-api_key = config["openai"]["api_key"]
-model = config["openai"]["model"]
-max_tokens = config["openai"]["max_tokens"]
 
-chat = ChatGPT(api_key, model, max_tokens = max_tokens)
+def ask_chatgpt(question: str, config_path: str) -> List[str]:
+    config = load_config(config_path)
 
-answer_pages = chat.ask(question)
-#answer_pages = chat.ask_debug(question)
+    api_key = config["openai"]["api_key"]
+    model = config["openai"]["model"]
+    max_tokens = config["openai"]["max_tokens"]
+    temperature = config["openai"]["temperature"]
+    top_p = config["openai"]["top_p"]
 
-for page in answer_pages:
-    print(page,'\n')
+    chat_model = ChatGPT(api_key, model, max_tokens = max_tokens, temperature = temperature, top_p = top_p)
+    answer_pages = chat_model.ask(question)
 
+    return answer_pages
+
+
+def main() -> None:
+    question = sys.argv[1]
+    config_path = "config.toml"
+    answers = ask_chatgpt(question, config_path)
+
+    for answer in answers:
+        print('\n', answer, '\n')
+
+
+if __name__ == "__main__":
+    main()
--- chatgpt.py
+++ chatgpt.py
@@ -1,90 +1,74 @@
 import openai
-import re
+import time
+import requests
+import logging
 
 from string import Template
 
+logging.basicConfig(level=logging.INFO)
+
 class ChatGPT:
-    def __init__(self, api_key, model_engine, max_tokens = 2048):
+    def __init__(self, api_key, model_engine, max_tokens = 2048, temperature=0.6, top_p=1.0):
         openai.api_key = api_key
         self.model_engine = model_engine
         self.max_tokens = max_tokens
+        self.temperature = temperature
+        self.top_p = top_p
         self.messages = []
 
-    def _paginate(self, words_list, mtpp):
-        tpages = []
-        pages = []
-        mcpp = mtpp * 4 #maximum number of characters in a page
-        page = ''
-        carry_word = ''
-        header = Template('PART $pagenumber OF $totalpages:\n')
-        reserved_chars = 21
+    def _create_paginated_prompts(self, words_list, tokens_per_page):
+        paginated_prompts, prompt, leftover_word = [], '', ''
+        max_chars_per_page = tokens_per_page * 4 
+        reserved_chars = 21 
         for word in words_list:
-            wl = len(word) + 1 #to account for the extra space between words
-            if (len(page) + wl + len(carry_word) + reserved_chars) < mcpp:
-                if len(carry_word) > 0:
-                    page += carry_word
-                    carry_word = ''
-                page += f' {word}'
+            word_length = len(word) + 1 
+            if (len(prompt) + word_length + len(leftover_word) + reserved_chars) < max_chars_per_page:
+                prompt += leftover_word + f' {word}'
+                leftover_word = ''
             else:
-                tpages.append(page)
-                carry_word = f' {word}'
-                page = ''
-        tpages.append(page)
-        npages = len(tpages)
-        if npages > 1:
-            i = 0
-            for page in tpages:
-                i += 1
-                h = header.substitute(pagenumber = f'{i}', totalpages = f'{npages}')
-                page = h + page
-                pages.append(page)
-        else:
-            pages = tpages
-        return pages
+                paginated_prompts.append(prompt)
+                leftover_word = f' {word}'
+                prompt = ''
+        paginated_prompts.append(prompt)
 
-    def _paginate_input(self, prompt):
-        tokens = prompt.split()
-        return self._paginate(tokens, self.max_tokens)
+        for index, prompt in enumerate(paginated_prompts):
+            header = f'PART {index+1} OF {len(paginated_prompts)}:\n'
+            paginated_prompts[index] = header + prompt if len(paginated_prompts) > 1 else prompt
 
-    def _send_prompt(self, prompt):
-        self._chat_session('user', prompt)
-        print('Sending prompt: ', prompt)
-        response = openai.ChatCompletion.create(
-            model = self.model_engine,
-            messages = self.messages,
-            max_tokens = self.max_tokens,
-            n = 1,
-            stop = None,
-            temperature = 0.6,
-#            stream = True
-        )
-        self.messages.append(response['choices'][0]['message'])
-        text = response['choices'][0]['message']['content']
-        return text
+        return paginated_prompts
 
-    def _chat_session(self, role, content):
-        message = {'role': f'{role}', 'content': f'{content}'} 
-        self.messages.append(message)
+    def _split_prompt_into_words(self, prompt):
+        return self._create_paginated_prompts(prompt.split(), self.max_tokens)
 
-    def ask(self, prompt):
-        prompt_pages = self._paginate_input(prompt)
-        output_pages = []
-        if len(prompt_pages) > 1:
-            self._send_prompt('My prompt is split in multiple parts. Each part starts with a "PART NUMBER OF TOTAL:" string where NUMBER is the number of the part and TOTAL is the total amount of parts. Please consider all parts together as a single input.')
-        for prompt_page in prompt_pages:
-            response = self._send_prompt(prompt_page)
-        output_pages.append(response)
-        continue_asking = 1
-        while continue_asking == 1:
-            for c in response[-1:]:
-                if c == '.':
-                    continue_asking = 0
-                else:
-                    response = self._send_prompt(f'The answer was truncated or incomplete. Please continue from where it stopped.')
-                    output_pages.append(response)
-        return output_pages
+    def _get_chat_response(self, prompt):
+        print('---', '\n', 'Sending prompt: ', prompt, '\n')
+        
+        for i in range(5):  # Retry up to 5 times
+            try:
+                response = openai.ChatCompletion.create(
+                    model=self.model_engine,
+                    messages=[{'role': 'user', 'content': prompt}] + self.messages,
+                    max_tokens=self.max_tokens,
+                    temperature=self.temperature,
+                    top_p=self.top_p
+                )
+                self.messages.append(response['choices'][0]['message'])
+                return response['choices'][0]['message']['content']
+            except requests.exceptions.RequestException as e:
+                logging.error("Request failed with %s, retrying...", e)
+                time.sleep((2 ** i) + (random.randint(0, 1000) / 1000))  # Exponential backoff
+        raise Exception("API request failed after multiple attempts")
 
-    def ask_debug(self, prompt):
-        prompt_pages = self._paginate_input(prompt)
-        return prompt_pages
+    def ask(self, prompt, role='user'):
+        self.messages.append({'role': role, 'content': prompt})
+        paginated_prompts = self._split_prompt_into_words(prompt)
+        chat_responses = []
 
+        for paginated_prompt in paginated_prompts:
+            response = self._get_chat_response(paginated_prompt)
+            chat_responses.append(response)
+            while not response.strip().endswith('.'):
+                response = self._get_chat_response('Please continue the incomplete response.', role='user')
+                chat_responses.append(response)
+
+        return chat_responses
